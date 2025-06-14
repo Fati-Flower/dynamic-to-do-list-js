7bdc52b894b59a2a91e4dcd3970014e551b800f4
Fatima Zahra ALOU <faalo9292@gmail.com>
2025-06-14 23:21:13 +0100

    script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ document.addEventListener('DOMContentLoaded', function () {
     const addButton = document.getElementById('add-task-btn');
     const taskInput = document.getElementById('task-input');
     const taskList = document.getElementById('task-list');
-
+    
     function addTask() {
         const taskText = taskInput.value.trim();
 
